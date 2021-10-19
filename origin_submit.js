b1050671c036b04d6710af71bc5cf870d5031dfd
jamsinboss <jamsinboss@gmail.com>
2021-10-20 00:53:37 +0900

    Forms Combined (v27)

--- origin_submit.js
+++ origin_submit.js
@@ -2,10 +2,24 @@ let form = document.getElementById("form");
 let url = "https://script.google.com/macros/s/AKfycbypwTB8D625tAwUUNW36c194ZxQk2xzBoD1co1tfZcrJ9Em_nhbRyLMTj9s0xHqfg/exec";
 
 let values = [];
+let grade = getParameterByName('grade');
+
+function recaptchaCallback() {
+    console.log('recaptcha succeed!');
+    isWaiting = false;
+    setTimeout(function() {
+        document.getElementById('recaptcha').style.visibility= 'hidden';
+    },1000);
+    while(waiting.length > 0)
+    {
+        appendForm(waiting[0]);
+        waiting.shift();
+    }
+}
 
 $.ajax({
 
-    url: encodeURI(url + "?type=getitem&grade=" + form.name),
+    url: encodeURI(url + "?type=getitem&grade=" + grade),
     type: 'GET',
     crossDomain: true,
     success: function(data) {
@@ -28,7 +42,27 @@ form.addEventListener("submit", function(e) {
     document.getElementById('submit').style.display = 'none';
     document.getElementById('submitting').style.display = '';
 
-    fetch(encodeURI(link),{
+    
+    $.ajax({
+
+        url: encodeURI(link),
+        type: 'GET',
+        crossDomain: true,
+        success: function(data) {
+            console.log(data);
+            if(data.error != undefined)
+                alert("오류가 발생했습니다. 현상이 지속된다면 jamsinboss@gmail.com으로 알려주세요.\n\n" + data.error);
+            else
+                alert("신청곡이 제출되었습니다. 감사합니다!");
+            location.href = "/index.html";
+        },
+        error: function(err) {
+            console.log(err);
+            alert("오류가 발생했습니다. 현상이 지속된다면 jamsinboss@gmail.com으로 알려주세요.\n\n" + err); 
+        }
+    });
+
+    /*fetch(encodeURI(link),{
         method: "GET",
         mode: "no-cors"
     })
@@ -40,12 +74,25 @@ form.addEventListener("submit", function(e) {
     .catch(function(err) {
         console.error(err)
         alert(err);
-    }); //promise based
+    }); //promise based*/
 })
 
 function handleData(data) {
-    for(let i=0;i<data.length;i++) {
-        const item = data[i];
+    const title = data.title;
+    const description = data.description;
+    appendForm('<h1>' + title + "</h1>");
+    appendForm('<ul>');
+    const des = description.split('\n');
+    for(let i=0;i<des.length;i++) {
+        appendForm('<li>' + des[i] + "</li>");
+    }
+    appendForm('</ul><br/><br/>');
+
+    document.title = title;
+
+    const list = data.items;
+    for(let i=0;i<list.length;i++) {
+        const item = list[i];
         putItem(item);
         values.push(item.id);
     }
@@ -119,25 +166,74 @@ let waiting = [];
 let isWaiting = true;
 function appendForm(str) {
     if(isWaiting) waiting.push(str);
-    else form.insertAdjacentHTML('beforeend', str);
+    else form.insertAdjacentHTML('beforeend', markdown(str));
 }
 
-function recaptchaCallback() {
-    console.log('recaptcha succeed!');
-    isWaiting = false;
-    setTimeout(function() {
-        document.getElementById('recaptcha').style.visibility= 'hidden';
-    },1000);
-    while(waiting.length > 0)
-    {
-        appendForm(waiting[0]);
-        waiting.shift();
+function markdown(str = String) {
+    str = str.replaceAll(/[\*]{2}([^\*\*]+)[\*]{2}/g, "<b>$1</b>");
+    str = str.replaceAll(/[\_]{2}([^\*\*]+)[\_]{2}/g, "<u>$1</u>");
+    str = str.replaceAll(/[\~]{2}([^\*\*]+)[\~]{2}/g, "<del>$1</del>");
+    str = str.replaceAll(/[\_]{1}([^\*\*]+)[\_]{1}/g, "<i>$1</i>");
+    str = str.replaceAll(/[\*]{1}([^\*\*]+)[\*]{1}/g, "<i>$1</i>");
+
+    return str;
+    /*let bold = false;
+    let italic = false;
+    let underlined = false;
+    for(var i=0;i<str.length;i++) {
+        console.log(str.substring(i,i+2));
+        if(str.substring(i,i+2) == "**")
+        {
+            if(bold === false)
+            {
+                str.replaceAt(i,"<strong>");
+                bold = true;
+                i+=6;
+            }
+            else
+            {
+                str.replaceAt(i,"</strong>");
+                bold = false;
+                i+=7;
+            }
+        }
+        else if(str.substring(i,i+2) == "__")
+        {
+            if(underlined === false)
+            {
+                str.replaceAt(i,"<u>");
+                underlined = true;
+                i+=2;
+            }
+            else
+            {
+                str.replaceAt(i,"</u>");
+                underlined = false;
+                i+=3;
+            }
+        }
+        else if(str.substring(i,i+1) == "*" || str.substring(i,i+1) == "_")
+        {
+            if(italic === false)
+            {
+                str.replaceAt(i,"<i>");
+                italic = true;
+                i+=2;
+            }
+            else
+            {
+                str.replaceAt(i,"</i>");
+                italic = false;
+                i+=3;
+            }
+        }
     }
+    return str;*/
 }
 
 function genSubmitUrl(){
     let link = url;
-    link += "?grade=" + form.name;
+    link += "?type=submit&grade=" + getParameterByName('grade');
     for(let i=0;i<values.length;i++)
     {
         console.log(i);
@@ -182,14 +278,15 @@ String.format = function() {
         return typeof(args[num]) != undefined ? args[num] : match;
     });
 }
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
 
-function getParam(sname) {
-    var params = location.search.substr(location.search.indexOf("?") + 1);
-    var sval = "";
-    params = params.split("&");
-    for (var i = 0; i < params.length; i++) {
-        temp = params[i].split("=");
-        if ([temp[0]] == sname) { sval = temp[1]; }
-    }
-    return sval;
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
